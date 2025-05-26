ab64b15597868e9be42bfda63a2ccaf91a80fa80
DyEsSuCr <2000sajo@gmail.com>
2025-05-26 00:38:17 -0500

    add: temporal custon logging

--- src/middleware.py
+++ src/middleware.py
@@ -1,11 +1,31 @@
+import time
+import logging
+
 from fastapi import FastAPI
+from fastapi.requests import Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.trustedhost import TrustedHostMiddleware
 
 from .settings import settings
 
+logger = logging.getLogger('uvicorn.access')
+logger.disabled = True
+
 
 def register_middleware(app: FastAPI):
+    @app.middleware('http')
+    async def custom_logging(request: Request, call_next):
+        start_time = time.time()
+
+        response = await call_next(request)
+        processing_time = time.time() - start_time
+        formatted_time = round(processing_time, 5)
+
+        message = f'{request.client.host}:{request.client.port} - {request.method} - {request.url.path} - {response.status_code} completed after {formatted_time}s'
+
+        print(message)
+        return response
+
     app.add_middleware(
         CORSMiddleware,
         allow_origins=[str(origin) for origin in settings.CORS_ORIGINS],
